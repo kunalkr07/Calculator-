b3c517ce0f8ae8f656936db1ad06c218f1774620
kunalWebDev07 <168627238+kunalWebDev07@users.noreply.github.com>
2024-06-04 14:32:05 +0530

    New calculator

--- calculator.js
+++ calculator.js
@@ -11,31 +11,32 @@ let num1=parseInt(prompt("Enter first number:"));
 let num2=parseInt(prompt("Enter second number:"));
 let total;
 
-switch(userNumber){
-    case 1:
-     total=num1+num2;
-     console.log(`The Addition of ${num1} + ${num2} = ${total}`);
-     break;
-
-    case 2:
-     total=num1-num2;
-     console.log(`The Substraction of ${num1} - ${num2} = ${total}`);
-     break;
-
-    case 3:
-            total=num1/num2;
-            console.log(`The Division of ${num1} / ${num2} = ${total}`);
+    switch(userNumber){
+        case 1:
+            total=num1+num2;
+            console.log(`The Addition of ${num1} + ${num2} = ${total}`);
             break;
-    case 4:
-     total=num1*num2;
-     console.log(`The Multiplication of ${num1} * ${num2} = ${total}`);
-     break;
-
-    case 5:
-     total=num1%num2;
-     console.log(`The Modulos of ${num1} % ${num2} = ${total}`);
-     break;
-
-     default:
-        console.log("Invalid choice!\nTry again");
-}
+       
+           case 2:
+            total=num1-num2;
+            console.log(`The Substraction of ${num1} - ${num2} = ${total}`);
+            break;
+       
+           case 3:
+                   total=num1/num2;
+                   console.log(`The Division of ${num1} / ${num2} = ${total}`);
+                   break;
+           case 4:
+            total=num1*num2;
+            console.log(`The Multiplication of ${num1} * ${num2} = ${total}`);
+            break;
+       
+           case 5:
+            total=num1%num2;
+            console.log(`The Modulos of ${num1} % ${num2} = ${total}`);
+            break;
+       
+            default:
+               console.log("Invalid choice!\nTry again");
+    }
+    
